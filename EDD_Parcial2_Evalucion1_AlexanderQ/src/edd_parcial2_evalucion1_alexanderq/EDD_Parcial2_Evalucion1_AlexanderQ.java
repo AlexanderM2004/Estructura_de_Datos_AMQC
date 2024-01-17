4d6e385a92610b925cfdc0f143829a86751ffe48
AlexanderM2004 <132873838+AlexanderM2004@users.noreply.github.com>
2024-01-17 10:57:04 -0500

    17/01/2024
    Evaluación 1 Segundo Parcial

---
+++ EDD_Parcial2_Evalucion1_AlexanderQ/src/edd_parcial2_evalucion1_alexanderq/EDD_Parcial2_Evalucion1_AlexanderQ.java
@@ -0,0 +1,12 @@
+package edd_parcial2_evalucion1_alexanderq;
+
+/**
+ *
+ * @author Alexander Quizhpe
+ */
+public class EDD_Parcial2_Evalucion1_AlexanderQ {
+    public static void main(String[] args) {
+        GestionNotas notas = new GestionNotas();//Se instancia un objeto de la clase Gestion Notas, para el cual se va a poder visibilizar el formulario
+        notas.setVisible(true);
+    } 
+}
---
+++ EDD_Parcial2_Evalucion1_AlexanderQ/src/edd_parcial2_evalucion1_alexanderq/GestionNotas.java
@@ -0,0 +1,363 @@
+package edd_parcial2_evalucion1_alexanderq;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.Arrays;
+/**
+ *
+ * @author Alexander Quizhpe
+ */
+public class GestionNotas extends JFrame{
+    public JPanel panel;
+    JTextField txt_number = new JTextField();
+    JTextField txt_name = new JTextField();
+    JTextArea resultArea = new JTextArea();
+    JButton AddButton = new JButton("Agregar");
+    JButton OrderBurbuja = new JButton("Ordenamiento Burbuja");
+    JButton OrderQuickSort = new JButton("Ordenamiento QuickSort");
+    JButton SearchSeccuencial = new JButton("Busqueda Secuencial");
+    JButton SearchBinario = new JButton("Busqueda Binaria");
+    private String[] nombresEstudiantes;
+    private int[] notasEstudiantes;
+    
+    //Creamos una ventana grafica con JFrame
+    public GestionNotas (){
+        AddButton.addActionListener((ActionEvent e) -> {
+            addRegister();
+        });
+        
+        OrderBurbuja.addActionListener((ActionEvent e) -> {
+            simulateBubbleSort();
+        });
+        
+        SearchSeccuencial.addActionListener((ActionEvent e) -> {
+            searchSecuencialName();
+        });
+        
+        OrderQuickSort.addActionListener((ActionEvent e) -> {
+            simulateQuickSort();
+        });
+        
+        SearchBinario.addActionListener((ActionEvent e) -> {
+            searchBinaryName();
+        });
+       this.setSize(680,450); 
+       setTitle("Algoritmos de Ordenamiento y Busqueda"); //Comando que permitecolocar nombre a la ventana
+       //Comando que permite finalizar todo el programa en el momento que se cierra la ventana
+       setDefaultCloseOperation(EXIT_ON_CLOSE);
+       //Comando para centrar la ventana del monitor
+       setLocationRelativeTo(null);
+       iniciarComponentes();
+    }
+
+    private void iniciarComponentes(){
+        colocarPanel();
+        colocarEtiquetas();
+        ColocarBotones();
+        ColocarCajatexto();
+        ColocarAreatext();
+    }
+
+    private void colocarPanel(){
+        panel = new JPanel();
+        //Comando para aplicar color al panel
+        panel.setBackground(Color.gray);
+        //Poner el panel sobre la ventana del registro
+        this.getContentPane().add(panel);
+    }
+
+    public void colocarEtiquetas(){
+        JLabel label = new JLabel("Algoritmos de Busqueda y Ordenacion",SwingConstants.CENTER);
+        panel.add(label);
+        panel.setLayout(null);
+        label.setBounds(175, 10, 350, 20);
+        label.setFont(new Font("Arial", Font.PLAIN, 15));
+        label.setForeground(Color.white);
+        
+        JLabel label1 = new JLabel();
+        label1.setText("Nombre: ");
+        panel.add(label1);
+        //Comando para ubicar el label
+        label1.setHorizontalAlignment(HEIGHT);
+        //comando para modificar fuente de letra
+        label1.setFont(new Font("Arial", Font.PLAIN, 15));
+        label1.setBounds(35, 45, 70, 15);
+        label1.setForeground(Color.white);
+        
+        JLabel label2 = new JLabel();
+        label2.setText("Promedio: ");
+        panel.add(label2);
+        //Comando para ubicar el label
+        label2.setHorizontalAlignment(HEIGHT);
+        //comando para modificar fuente de letra
+        label2.setFont(new Font("Arial", Font.PLAIN, 15));
+        label2.setBounds(25, 70, 80, 15);
+        label2.setForeground(Color.white);
+        
+        JLabel label3 = new JLabel();
+        label3.setText("Rango de notas de 0 - 20** ");
+        panel.add(label3);
+        //Comando para ubicar el label
+        label3.setHorizontalAlignment(HEIGHT);
+        //comando para modificar fuente de letra
+        label3.setFont(new Font("Arial", Font.PLAIN, 10));
+        label3.setBounds(160, 70, 250, 15);
+        label3.setForeground(Color.white);
+        
+        JLabel label4 = new JLabel();
+        label4.setText("Nota: Las Busquedas se las hace ");
+        panel.add(label4);
+        //Comando para ubicar el label
+        label4.setHorizontalAlignment(HEIGHT);
+        //comando para modificar fuente de letra
+        label4.setFont(new Font("Arial", Font.PLAIN, 15));
+        label4.setBounds(35, 325, 250, 15);
+        label4.setForeground(Color.white);
+        
+        JLabel label5 = new JLabel();
+        label5.setText("solo por nombre ");
+        panel.add(label5);
+        //Comando para ubicar el label
+        label5.setHorizontalAlignment(HEIGHT);
+        //comando para modificar fuente de letra
+        label5.setFont(new Font("Arial", Font.PLAIN, 15));
+        label5.setBounds(75, 350, 250, 15);
+        label5.setForeground(Color.white);
+    }
+
+    //Comando para colocar cajas de texto
+    public void ColocarCajatexto(){
+        txt_name.setBounds(105, 43, 200, 20);
+        panel.add(txt_name);
+        txt_number.setBounds(105, 67, 50, 20);
+        panel.add(txt_number);
+    }
+
+    public void ColocarAreatext(){
+        resultArea.setLayout(null);
+        resultArea.setBounds(320, 43, 300, 340);
+        resultArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(resultArea);
+        scrollPane.setBounds(320, 43, 300, 340);
+        panel.add(resultArea);
+        panel.add(scrollPane);
+    }
+
+    public void ColocarBotones(){
+        AddButton.setBounds(80, 100, 175, 30);
+        panel.add(AddButton);
+        AddButton.setEnabled(true);//Sirve para habilitar o deshabilitar los botones
+        AddButton.setMnemonic('a');//Sirve para que se active el boton con el teclado Alt + A
+        
+        OrderBurbuja.setBounds(80, 140, 175, 30);
+        panel.add(OrderBurbuja);
+        OrderBurbuja.setEnabled(true);//Sirve para habilitar o deshabilitar los botones
+        OrderBurbuja.setMnemonic('o');//Sirve para que se active el boton con el teclado Alt + O
+        
+        SearchSeccuencial.setBounds(80, 180, 175, 30);
+        panel.add(SearchSeccuencial);
+        SearchSeccuencial.setEnabled(true);//Sirve para habilitar o deshabilitar los botones
+        SearchSeccuencial.setMnemonic('S');//Sirve para que se active el boton con el teclado Alt + S
+        
+        OrderQuickSort.setBounds(80, 220, 175, 30);
+        panel.add(OrderQuickSort);
+        OrderQuickSort.setEnabled(true);//Sirve para habilitar o deshabilitar los botones
+        OrderQuickSort.setMnemonic('q');//Sirve para que se active el boton con el teclado Alt + Q
+        
+        SearchBinario.setBounds(80, 260, 175, 30);
+        panel.add(SearchBinario);
+        SearchBinario.setEnabled(true);//Sirve para habilitar o deshabilitar los botones
+        SearchBinario.setMnemonic('B');//Sirve para que se active el boton con el teclado Alt + Q
+    }
+    
+    private void addRegister(){
+        String nombre = txt_name.getText();
+        String notaText = txt_number.getText();
+
+        if (!nombre.isEmpty() && !notaText.isEmpty()) {
+            int nota = Integer.parseInt(notaText);
+            if((nota >= 0)&&(nota <=20)){
+                if (nombresEstudiantes == null) {
+                    nombresEstudiantes = new String[]{nombre};
+                    notasEstudiantes = new int[]{nota};
+                } else {
+                    // Añadir nuevos elementos a los arreglos
+                    nombresEstudiantes = Arrays.copyOf(nombresEstudiantes, nombresEstudiantes.length + 1);
+                    notasEstudiantes = Arrays.copyOf(notasEstudiantes, notasEstudiantes.length + 1);
+
+                    nombresEstudiantes[nombresEstudiantes.length - 1] = nombre;
+                    notasEstudiantes[notasEstudiantes.length - 1] = nota;
+                }
+                mostrarResultado("Nombre: " + nombre + "    Promedio: " + notaText);
+            }else{
+                JOptionPane.showMessageDialog(this, "Registro no permitido, rango de notas permitidas de 0 a 20","Error de ingreso de Registro", JOptionPane.ERROR_MESSAGE);
+            }
+        } else {
+            mostrarResultado("Por favor, ingrese el nombre y la nota del estudiante.");
+        }
+        // Limpiar campos después de ingresar una nota
+        txt_name.setText("");
+        txt_number.setText("");
+    }
+    
+    private void sort() {
+        //Se declara un método estático llamado sort que toma un arreglo de enteros como parámetro. Este método implementa el algoritmo de ordenamiento de burbuja.
+        mostrarResultado("--------------Ordenano con Algoritmo Burbuja--------------");    
+        int n = notasEstudiantes.length;
+        boolean intercambio;
+        do {
+            intercambio = false;
+            for (int i = 1; i < n; i++) {
+                if (notasEstudiantes[i - 1] > notasEstudiantes[i]) {
+                    // Intercambiar notas
+                    int tempNota = notasEstudiantes[i - 1];
+                    notasEstudiantes[i - 1] = notasEstudiantes[i];
+                    notasEstudiantes[i] = tempNota;
+
+                    // Intercambiar nombres
+                    String tempNombre = nombresEstudiantes[i - 1];
+                    nombresEstudiantes[i - 1] = nombresEstudiantes[i];
+                    nombresEstudiantes[i] = tempNombre;
+
+                    intercambio = true;
+
+                    // Mostrar visualmente el proceso de ordenación paso a paso
+                    mostrarResultado(Arrays.toString(nombresEstudiantes) + "\n" + Arrays.toString(notasEstudiantes));
+                }
+            }
+        } while (intercambio);
+    }
+     
+    private void simulateBubbleSort(){
+        //Verifica si hay datos el la caja de texto correspondiente para poder empezar a ordenar las notas
+        if (nombresEstudiantes != null && nombresEstudiantes.length > 1) {
+            sort();
+            mostrarResultado("Notas ordenadas correctamente.");
+        } else {
+            mostrarResultado("Ingrese al menos dos notas para ordenar.");
+        }
+    }
+    
+    public int SecuencialName(String nombreBuscar){
+        //Codigo de busqueda secuencial
+        //Iterador
+        // Iterar sobre el arreglo
+        for (int i = 0; i < nombresEstudiantes.length; i++) {
+            if (nombresEstudiantes[i].equalsIgnoreCase(nombreBuscar)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    //Metodo que permite hacer la busqueda Secuencial
+    private void searchSecuencialName(){
+        //Verifica si hay datos el la caja de texto correspondiente para poder realizar la busqueda
+        String nombreBuscar = txt_name.getText();
+        mostrarResultado("------------Busqueda con Algoritmo Secuencial-------------"); 
+        if (!nombreBuscar.isEmpty() && nombresEstudiantes != null) {
+            int i = SecuencialName(nombreBuscar);
+
+            if (i != -1) {
+                mostrarResultado("Estudiante encontrado: " + nombresEstudiantes[i] +",  Nota: " + notasEstudiantes[i]);
+            } else {
+                mostrarResultado("Estudiante no encontrado.");
+            }
+        } else {
+            mostrarResultado("Ingrese el nombre del estudiante para buscar.");
+        }
+    }
+    //Se aplica temas de recursivaidad para que una funcion este llamando a otra omitiendo o ahporrando la codificacion de varias de codigo
+    private void OrderQuickSort(){
+        mostrarResultado("-------------Ordenano con Algoritmo QuickSort-------------"); 
+        quickSort(notasEstudiantes, 0, notasEstudiantes.length-1);//Metodo que llama al metodo quickSort en el cual pasa datos como el arreglo que se va a ordenar, el elemento inial y el ultimo elemto del arreglo
+    }
+    
+    public void quickSort(int[] arr, int izq, int der) {
+        if (izq < der) {
+            int pivotIndex = partition(notasEstudiantes, izq, der);//Determine el elementp pivote del arreglo en cada pasada
+            quickSort(notasEstudiantes, izq, pivotIndex - 1);//Metodo para trabajar con el lado izquierdo de arreglo (Subarreglo izquierdo)
+            quickSort(notasEstudiantes, pivotIndex + 1, der);//Metodo para trabajar con el lado derecho del arreglo (Subarreglo derecho)
+        }
+    }
+    //Metodo que permite cambiar de posicion a los datos
+    public int partition(int[] arr, int izqu, int dere) {
+        int pivot = notasEstudiantes[dere];
+        int i = izqu - 1;
+        //Permite cambiar de posicion con el subarreglo izquierdo
+        for (int j = izqu; j < dere; j++) {
+            if (notasEstudiantes[j] < pivot) {
+                i++;
+                int temp = notasEstudiantes[i];
+                notasEstudiantes[i] = notasEstudiantes[j];
+                notasEstudiantes[j] = temp;
+                
+                String tempNomb = nombresEstudiantes[i];
+                nombresEstudiantes[i] = nombresEstudiantes[j];
+                nombresEstudiantes[j] = tempNomb;
+                // Mostrar visualmente el proceso de ordenación paso a paso
+                mostrarResultado(Arrays.toString(nombresEstudiantes) + "\n" + Arrays.toString(notasEstudiantes));
+            }
+        }
+        //Permite cambiar de posicion el subarreglo derecho
+        int temp = notasEstudiantes[i + 1];
+        notasEstudiantes[i + 1] = notasEstudiantes[dere];
+        notasEstudiantes[dere] = temp;
+        
+        String tempNomb = nombresEstudiantes[i + 1];
+        nombresEstudiantes[i + 1] = nombresEstudiantes[dere];
+        nombresEstudiantes[dere] = tempNomb;
+        // Mostrar visualmente el proceso de ordenación paso a paso
+        mostrarResultado(Arrays.toString(nombresEstudiantes) + "\n" + Arrays.toString(notasEstudiantes));
+        return i + 1;
+    }    
+    //Permite utilizar los metodos correspondientes para el uso del ordenamientto por QuickSort
+    private void simulateQuickSort(){
+        //Verifica si hay datos el la caja de texto correspondiente para poder empezar a ordenar las notas
+        if (nombresEstudiantes != null && nombresEstudiantes.length > 1) {
+            OrderQuickSort();
+            mostrarResultado("Notas ordenadas correctamente.");
+        } else {
+            mostrarResultado("Ingrese al menos dos notas para ordenar.");
+        }
+    }
+    
+    //Metodo que permite realizar la busqueda Binaria
+    public int BinaryName(String nomBuscar){
+        int izqui = 0;
+        int derec = nombresEstudiantes.length - 1;
+        
+        while (izqui <= derec) {
+            int mid = izqui + (derec - izqui) / 2;
+            int res = nomBuscar.compareTo(nombresEstudiantes[mid]);
+            if (res == 0) {
+                return mid;
+            } else if (res > 0) {
+                izqui = mid + 1;
+            } else {
+                derec = mid - 1;
+            }
+        }
+        return -1;
+    }
+    //Metodo que permite llamar a la busqueda Binaria, con nombres
+    private void searchBinaryName(){
+        //Verifica si hay datos el la caja de texto correspondiente para poder realizar la busqueda
+        String nomBuscar = txt_name.getText();
+        mostrarResultado("------------Busqueda con Algoritmo Binario-------------"); 
+        if (!nomBuscar.isEmpty() && nombresEstudiantes != null) {
+            int i = BinaryName(nomBuscar);
+
+            if (i != -1) {
+                mostrarResultado("Estudiante encontrado: " + nombresEstudiantes[i] +",  Nota: " + notasEstudiantes[i]);
+            } else {
+                mostrarResultado("Estudiante no encontrado.");
+            }
+        } else {
+            mostrarResultado("Ingrese el nombre del estudiante para buscar.");
+        }
+    }
+    //Permite mostrar los resultados por el Area Text
+    private void mostrarResultado(String mensaje) {
+        resultArea.append(mensaje + "\n\n");
+    }
+}
