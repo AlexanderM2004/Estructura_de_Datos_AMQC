decf602f3d733fb191a92a75000f0730b5bd38c6
AlexanderM2004 <132873838+AlexanderM2004@users.noreply.github.com>
2024-01-17 23:59:46 -0500

    17/01/2024
    Segundo Parcial
    Mejora con MONGO DB

---
+++ EDD_Parcial2_Evalucion1_MongoDB_AlexanderQ/src/edd_parcial2_evalucion1_alexanderq/DB_MongoDB.java
@@ -0,0 +1,28 @@
+package edd_parcial2_evalucion1_alexanderq;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alexander Quizhpe
+ */
+public class DB_MongoDB {
+    //DB - CONSTRUCTOR
+    public MongoDatabase ConnectDB (){
+        MongoDatabase database = null;
+        int puerto = 27017;
+        String server = "localhost";
+        
+        try {
+            MongoClient query = new MongoClient(server, puerto);
+            database = query.getDatabase("GestionNotas");
+            System.out.println("MONGODB-CONNECT");
+            return database;
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Hubo un error al conectar.");
+        }
+        return database;
+    }
+}
